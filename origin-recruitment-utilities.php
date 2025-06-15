100230bcf39ea964590c80e473ab62bef7c71977
Patrick Appelman <patrick@appelmandesigns.com>
2025-06-15 07:12:51 -0500

    feat: create custom post type job

--- origin-recruitment-utilities.php
+++ origin-recruitment-utilities.php
@@ -11,58 +11,58 @@
 
 /* DEFINE CUSTOM POST TYPES */
 
-// function or_create_posttypes() {
+function or_create_posttypes() {
 
-//   // Register custom post type: Jobs
+  // Register custom post type: Jobs
 
-//   register_post_type( 'job',
-//     array(
-//       'labels' => array(
-//         'name' => __( 'Jobs' ),
-//         'singular_name'         => __( 'Job' ),
-//         'menu_name'             => __( 'Jobs', 'Admin Menu text', 'textdomain' ),
-//         'name_admin_bar'        => __( 'Job', 'Add New on Toolbar', 'textdomain' ),
-//         'add_new'               => __( 'Add New', 'textdomain' ),
-//         'add_new_item'          => __( 'Add Job', 'textdomain' ),
-//         'new_item'              => __( 'New Job', 'textdomain' ),
-//         'edit_item'             => __( 'Edit Job', 'textdomain' ),
-//         'view_item'             => __( 'View Job', 'textdomain' ),
-//         'all_items'             => __( 'All Jobs', 'textdomain' ),
-//         'search_items'          => __( 'Search Jobs', 'textdomain' ),
-//         'parent_item_colon'     => __( 'Parent Jobs:', 'textdomain' ),
-//         'not_found'             => __( 'No jobs found.', 'textdomain' ),
-//         'not_found_in_trash'    => __( 'No jobs found in Trash.', 'textdomain' ),
-//         'featured_image'        => _x( 'Job Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
-//         'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-//         'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-//         'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-//         'archives'              => _x( 'Job archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
-//         'insert_into_item'      => _x( 'Insert into job', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
-//         'uploaded_to_this_item' => _x( 'Uploaded to this job', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
-//         'filter_items_list'     => _x( 'Filter jobs list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
-//         'items_list_navigation' => _x( 'Jobs list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
-//         'items_list'            => _x( 'Jobs list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
-//       ),
-//       'description'        => 'A Wordpress custom post type for jobs.',
-//       'public'             => true,
-//       'publicly_queryable' => true,
-//       'show_ui'            => true,
-//       'supports'           => array( 'title', 'editor', 'excerpt', 'custom-fields' ),
-//       'show_in_rest'       => true,
-//       'hierarchical'       => false,
-//       'rewrite'            => array('slug' => 'jobs', 'with_front' => false),
+  register_post_type( 'job',
+    array(
+      'labels' => array(
+        'name' => __( 'Jobs' ),
+        'singular_name'         => __( 'Job' ),
+        'menu_name'             => __( 'Jobs', 'Admin Menu text', 'textdomain' ),
+        'name_admin_bar'        => __( 'Job', 'Add New on Toolbar', 'textdomain' ),
+        'add_new'               => __( 'Add New', 'textdomain' ),
+        'add_new_item'          => __( 'Add Job', 'textdomain' ),
+        'new_item'              => __( 'New Job', 'textdomain' ),
+        'edit_item'             => __( 'Edit Job', 'textdomain' ),
+        'view_item'             => __( 'View Job', 'textdomain' ),
+        'all_items'             => __( 'All Jobs', 'textdomain' ),
+        'search_items'          => __( 'Search Jobs', 'textdomain' ),
+        'parent_item_colon'     => __( 'Parent Jobs:', 'textdomain' ),
+        'not_found'             => __( 'No jobs found.', 'textdomain' ),
+        'not_found_in_trash'    => __( 'No jobs found in Trash.', 'textdomain' ),
+        'featured_image'        => _x( 'Job Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'archives'              => _x( 'Job archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+        'insert_into_item'      => _x( 'Insert into job', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+        'uploaded_to_this_item' => _x( 'Uploaded to this job', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+        'filter_items_list'     => _x( 'Filter jobs list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+        'items_list_navigation' => _x( 'Jobs list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+        'items_list'            => _x( 'Jobs list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+      ),
+      'description'        => 'A Wordpress custom post type for jobs.',
+      'public'             => true,
+      'publicly_queryable' => true,
+      'show_ui'            => true,
+      'supports'           => array( 'title', 'editor', 'excerpt', 'custom-fields' ),
+      'show_in_rest'       => true,
+      'hierarchical'       => false,
+      'rewrite'            => array('slug' => 'jobs', 'with_front' => false),
 
-//       'capability_type'    => 'post',
-//       'taxonomies'         => array('country'),
-//       'has_archive'        => false,
-//       'menu_icon'          => 'dashicons-businessman',
+      'capability_type'    => 'post',
+      'taxonomies'         => array('country'),
+      'has_archive'        => false,
+      'menu_icon'          => 'dashicons-businessman',
       
-//     )
-//   );
+    )
+  );
 
-// }
+}
 
-// add_action( 'init', 'or_create_posttypes' );
+add_action( 'init', 'or_create_posttypes' );
 
 
 
